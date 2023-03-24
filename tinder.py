ccbaf0779413ba4a0decdb04367668f19ba11818
TheProgrammer <mxolisinkosi2360@gmail.com>
2023-03-24 05:44:14 +0200

    imported the need modules to fully run the TinderBot

--- tinder.py
+++ tinder.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+import time
 
 
 class TinderBot:
@@ -13,11 +17,13 @@ class TinderBot:
 
     def __login_with_facebook(self) -> None:
         """will click the login with facebook: helper method"""
+
     def __accept_cookies(self) -> None:
         """clicks the accept cookies when the notification shows up after logging in: helper method"""
 
     def login(self, username: str, password: str) -> None:
         """will log in the user with using their facebook credentials"""
+        time.sleep(3600)
 
     def swiper(self, total: int, like: int, dislike: int) -> None:
         """will swipe left and right in proportion and until the 'total' number is reached"""
