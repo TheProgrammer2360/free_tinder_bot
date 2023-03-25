093e6c54810b29e82eae41bb698733e992e9dc12
TheProgrammer <mxolisinkosi2360@gmail.com>
2023-03-25 16:28:29 +0200

    added more functionality on the main app

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from tinder import TinderBot
 import os
 
 if __name__ == '__main__':
+    print("Welcome to the Tinder Swiper")
+    total = int(input("Enter the number of total swipes that you would like: "))
+    like = int(input("Enter the number of likes you would like to do before disliking: "))
+    dislike = int(input("Enter the number of dislikes you would do before going to like again: "))
     bot = TinderBot()
     # login using facebook details
     bot.login(username=os.environ["PHONENUMBER"], password=os.environ["PASSWORD"])
+    bot.swiper(total, like, dislike)
 
